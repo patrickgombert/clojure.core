6a58c33f19accfb3a5fdc6e23424df6cf8dc2c46
Kevin Buchanan <kevaustinbuch@gmail.com>
2015-08-21 15:01:17 -0500

    WIP

--- src/clj/clojure/lang/stm.clj
+++ src/clj/clojure/lang/stm.clj
@@ -1,5 +1,5 @@
 (ns clojure.lang.stm
-  (:refer-clojure :only [defn defn- deftype let binding])
+  (:refer-clojure :only [defn defn- deftype let binding doseq])
   (:require [clojure.lang.atomic-ref :refer [new-atomic-ref ref-set!
                                              ref-get ref-compare-and-set!
                                              new-atomic-long]]
@@ -52,40 +52,85 @@
   (get-info [this])
 
   (run [this fn]
-    (loop [i 0 done false ret nil]
-      (set! read-point (get-and-increment-atomic-counter last-point))
-      (if (= i 0)
-        (set! start-point read-point)
-        (set! start-time (nano-time)))
-      (set! info (new-info RUNNING start-point))
-      (let [ret (fn)]
-        (if (ref-compare-and-set! (.status info) RUNNING COMMITTING)
-          (loop [[ref fns :as c] (first commutes) cs (rest commutes)]
-            (when c
+    (let [done (atom nil)
+          ret (atom nil)]
+      (loop [i 0]
+        (try
+          (set! read-point (get-and-increment-atomic-counter last-point))
+          (when (= i 0)
+            (set! start-point read-point)
+            (set! start-time (nano-time)))
+          (set! info (new-info RUNNING start-point))
+          (reset! ret (fn))
+          (when (ref-compare-and-set! (.status info) RUNNING COMMITTING)
+            (doseq [[ref fns] commutes]
               (when-not (contains? sets ref)
                 (let [was-ensured (contains? ensures ref)]
                   (release-if-ensured ref)
                   (try-write-lock ref)
                   (add-lock locked ref)
                   (when (and was-ensured (tvals ref) (> (point (tvals ref)) read-point))
-                    (throw retry-ex))
-                  (let [refinfo (tinfo ref)]
-                    (when (and refinfo (not= refinfo info) (running? refinfo))
-                      (when-not (barge! refinfo)
-                        (throw retry-ex))))
-                  (let [val (when-let [tval (tvals ref)] (val tval))]
-                    (set! vals (assoc vals ref val)))
-                  (loop [ffn (first fns) rfns (rest fns)]
-                    (when ffn
-                      (set! vals (assoc vals ref (wtf)))
-                      (recur (first rfns) (rest rfns))))))
-              (recur (first cs) (rest cs))))
-          (loop [ref (first sets) rs (rest sets)]
-            (when ref
+                    (throw retry-ex)))
+                (let [refinfo (tinfo ref)]
+                  (when (and refinfo (not= refinfo info) (running? refinfo))
+                    (when-not (barge! refinfo)
+                      (throw retry-ex))))
+                (let [val (when-let [tval (tvals ref)] (val tval))]
+                  (set! vals (assoc vals ref val)))
+                (doseq [f fns]
+                  (set! vals (assoc vals ref (wtf))))))
+            (doseq [ref sets]
               (try-write-lock ref)
-              (add-lock locked ref)
-              (recur (first rs) (rest rs))))
-
+              (add-lock locked ref))
+            (doseq [[ref entry] vals]
+              (validate ref (get-validator ref) entry))
+            (let [commit-point (get-and-increment-atomic-counter last-point)]
+              (doseq [[ref entry] vals]
+                (let [oldval (when-let [tval (tvals ref)] (val tval))
+                      hcount (histcount ref)]
+                  (cond
+                    (not (tvals ref))
+                    (set! (tvals ref) (new-tval entry commit-point))
+                    (or (and (pos? (get-faults ref)) (< hcount (get-max-history ref)))
+                        (< hcount (get-min-history ref)))
+                    (set! (tvals ref) (new-tval entry commit-point (tvals ref)))
+                    :else
+                    (do
+                      (set! (tvals ref) (get-next (tvals ref)))
+                      (set! (val (tvals ref)) entry)
+                      (set! (point (tvals ref)) commit-point)))
+                  (when (seq (get-watches ref))
+                    (add-notify (new-notify ref oldval entry))))))
+            (reset! done true)
+            (set! (.status info) COMMITTED))
+          (catch retry-ex)
+          (finally
+            (doseq [i (range (count locked))]
+              (-> (get locked i)
+                  get-lock
+                  write-lock
+                  unlock))
+            (clear locked)
+            (doseq [ref ensures]
+              (-> (get-lock ref)
+                  read-lock
+                  unlock))
+            (clear ensures)
+            (stop (if @done COMMITTED RETRY))
+            (try
+              (if @done
+                (doseq [n notifies]
+                  (notify-watches (get-ref n) (get-oldval n) (get-newval n)))
+                (doseq [a actions]
+                  (dispatch-action a)))
+              (finally
+                (clear notifies)
+                (clear actions)))))
+        (if @done
+          @ret
+          (if (< i RETRY_LIMIT)
+            (recur (inc i))
+            (throw "Transaction failed after reaching retry limit")))))))
 
 (defn new-transaction []
   (Transaction.
